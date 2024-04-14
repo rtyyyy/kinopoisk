a8efa2fbe7fa91355074d4e3f5a125deeab20e4c
rtyyyy <1etudiant@mail.ru>
2024-04-15 02:43:33 +0300

    class request

--- src/App.php
+++ src/App.php
@@ -3,13 +3,14 @@ namespace App;
 
 //класс, отвечающий за запуск приложения//
 use App\Router\Router;
+use App\Http\Request;
+
 class App
 {
     public function run(): void
     {
         $router = new Router();
-        $uri = $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-        $router->dispatch($uri, $method);
+        $request = Request::createFromGlobals();
+        $router->dispatch($request->uri(),$request->method());
     }
 }
---
+++ src/Http/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http;
+//инкапсулируем запросы
+class Request
+{
+    public function __construct(
+        public readonly array $get,
+        public readonly array $post,
+        public readonly array $server,
+        public readonly array $files,
+        public readonly array $cookies,
+    )
+    {}
+    //создаёт инстанс  себя же для конструктора
+    public static function createFromGlobals(): static
+    {
+        return new static(
+            $_GET,
+            $_POST,
+            $_SERVER,
+            $_FILES,
+            $_COOKIE,
+        );
+    }
+    public function uri():string
+    {
+        return strtok($this->server['REQUEST_URI'], token:'?');
+    }
+    public function method():string
+    {
+        return $this->server['REQUEST_METHOD'];
+    }
+
+}
